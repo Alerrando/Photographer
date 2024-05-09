b63bee12b1fea71c45f9d5c9d56a31dd2eece47e
Alerrando <alerrando2@gmail.com>
2024-05-09 19:22:20 -0300

    feat: add initial layout of main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,23 @@ export default function Home() {
           </ul>
         </div>
       </header>
+
+      <main className="h-screen w-full p-4 mt-[12%]">
+        <div className="flex flex-col gap-2">
+          <div className="grid gap-1">
+            <span className="text-[10px] font-bold text-[#CD5FF8]">Fotos Excepcionais Sempre</span>
+            <h1 className="text-5xl font-bold">Capture os Momentos Preciosos Para Você</h1>
+          </div>
+
+          <span className="font-bold text-xs text-[#BDBDBD]">
+            {
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the \
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and \
+            scrambled it to make a type specimen book"
+            }
+          </span>
+        </div>
+      </main>
     </>
   );
 }
