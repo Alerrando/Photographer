106f19ac136675b35ca5e82e50d8a9cd143a2ba4
Alerrando <alerrando2@gmail.com>
2024-05-09 21:29:50 -0300

    feat: add link in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="w-full flex items-center justify-center fixed top-0 text-black bg-white z-10">
@@ -5,9 +7,13 @@ export default function Header() {
         <h2 className="text-2xl md:text-4xl font-bold">MIYUKI</h2>
 
         <ul className="flex items-center gap-8">
-          <li className="text-xs md:text-base font-bold">Home</li>
-          <li className="text-xs md:text-base">Gallery</li>
-          <li className="text-xs md:text-base">About</li>
+          <Link href="/">
+            <li className="text-xs md:text-base hover:font-bold">Home</li>
+          </Link>
+
+          <Link href="/gallery">
+            <li className="text-xs md:text-base hover:font-bold">Gallery</li>
+          </Link>
         </ul>
       </div>
     </header>
